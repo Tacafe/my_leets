a0f720ee7237a868320686f0be5bbdcd51327582
tacafe <kenjitacafe@gmail.com>
2022-10-27 08:53:43 +0900

    [solved] Problem 83: Easy

---
+++ leet_83/main.go
@@ -0,0 +1,28 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	var current *ListNode = head
+
+	for current != nil && current.Next != nil {
+		if current.Next.Val == current.Val {
+			current.Next = current.Next.Next
+		} else {
+			current = current.Next
+		}
+	}
+
+	return head
+}
+
+func main() {
+
+}
