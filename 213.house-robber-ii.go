23b7099fe29331da4c0103e7550dd59d154a3fee
tacafe <kenjitacafe@gmail.com>
2023-12-06 09:46:22 +0900

    solved more

--- 213.house-robber-ii.go
+++ 213.house-robber-ii.go
@@ -6,7 +6,28 @@
 
 // @lc code=start
 func rob(nums []int) int {
-    
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	return max(helper(nums, 0, len(nums)-2), helper(nums, 1, len(nums)-1))
 }
+
+func helper(nums []int, l, r int) int {
+	A, B := 0, 0
+	for i := l; i <= r; i++ {
+		a, b := A, B
+		A = b + nums[i]
+		B = max(a, b)
+	}
+	return max(A, B)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 // @lc code=end
 
